983fadf599c9e0ab94f5fcb0c915fba26d5e016f
Natalia Yukari Kawashisa <nataliakawashisa@gmail.com>
2025-07-02 17:00:42 -0300

    Working on LoginPage frontend

--- frontend/src/app/components/button-component/button-component.ts
+++ frontend/src/app/components/button-component/button-component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-button-component',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule
+  ],
   templateUrl: './button-component.html',
   styleUrl: './button-component.css'
 })
 export class ButtonComponent {
-
+  @Input() buttonLabel: string= '';
+  @Input() buttonType: string= '';
 }
